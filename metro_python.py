4d52243d96e4d701ee4af3759972526ab325c926
gabrielyanxing <xingy@metropolia.fi>
2024-08-22 11:21:45 +0300

    first commit

--- metro_python.py
+++ metro_python.py
@@ -76,18 +76,22 @@ else:
     print("Invalid cabin class.")
 
 # 3.3
-biological_gender = input("Enter your biological_gender:")
+biological_gender = input("Enter your biological_gender(female/male):")
 hemoglobin_value = float(input("Enter your hemoglobin value (g/l):"))
 if biological_gender == "female":
-    if hemoglobin_value >= 117 and hemoglobin_value <= 155:
-        print("Yuor hemoglobin value is normal!")
+    if hemoglobin_value < 117:
+        print("Your hemoglobin is LOW!")
+    elif hemoglobin_value > 155:
+        print("Your hemoglobin is HIGH!")
     else:
-        print("Yuor hemoglobin value is not normal!")
+        print("Your hemoglobin value is normal!")
 elif biological_gender == "male":
-    if hemoglobin_value >= 134 and hemoglobin_value <= 167:
-        print("Yuor hemoglobin value is normal!")
+    if hemoglobin_value < 134:
+        print("Your hemoglobin is LOW!")
+    elif hemoglobin_value > 167:
+        print("Your hemoglobin is HIGH!")
     else:
-        print("Yuor hemoglobin value is not normal!")
+        print("Your hemoglobin value is normal!")
 
 # 3.4
 year = int(input("Enter a year:"))
